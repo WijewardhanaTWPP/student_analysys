c2fd51e7cfe9d5615beaeb5c48637f0e3d91befa
WijewardhanaTWPP <99610562+WijewardhanaTWPP@users.noreply.github.com>
2026-02-02 09:22:50 +0530

    day 1

---
+++ edu-backend/src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+import studentsRoutes from "./routes/students.routes.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/health", (req, res) => res.json({ ok: true }));
+
+app.use("/api/students", studentsRoutes);
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`API running on http://localhost:${PORT}`));
